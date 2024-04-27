646b1eb964febcf8713cecfb5e767988d230d270
Zach <duong.nguyen@rochester.edu>
2024-04-27 12:50:28 -0400

    Fix some exports problem

--- src/lights/exports.ts
+++ src/lights/exports.ts
@@ -1,2 +1,3 @@
+export * from './Light.js';
 export * from './AmbientLight.js';
 export * from './PointLight.js';
--- src/materials/exports.ts
+++ src/materials/exports.ts
@@ -1,3 +1,4 @@
+export * from './constants.js';
 export * from './Material.js';
 export * from './SolidColor.js';
 export * from './BlinnPhong.js';
