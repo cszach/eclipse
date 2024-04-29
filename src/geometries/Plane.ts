c8ad68d5f69f742b7dab855025428b3101348a06
Zach <duong.nguyen@rochester.edu>
2024-04-29 10:00:50 -0400

    [geometries]: Make plane one-sided

--- src/geometries/Plane.ts
+++ src/geometries/Plane.ts
@@ -19,41 +19,39 @@ class Plane extends Geometry {
     //  |  \|     |---+X
     // v2--v3
 
-    // We want the plane to be double-sided, so for each vertex, make 2 copies;
-    // one for the front and one for the back.
     // prettier-ignore
     const vertices = new Float32Array([
        // v0
        halfWidth,  halfHeight, 0, // 0
-       halfWidth,  halfHeight, 0, // 1
+      //  halfWidth,  halfHeight, 0, // 1
 
        // v1
       -halfWidth,  halfHeight, 0, // 2
-      -halfWidth,  halfHeight, 0, // 3
+      // -halfWidth,  halfHeight, 0, // 3
 
        // v2
       -halfWidth, -halfHeight, 0, // 4
-      -halfWidth, -halfHeight, 0, // 5
+      // -halfWidth, -halfHeight, 0, // 5
 
        // v3
        halfWidth, -halfHeight, 0, // 6
-       halfWidth, -halfHeight, 0, // 7
+      //  halfWidth, -halfHeight, 0, // 7
     ]);
 
     // prettier-ignore
     const indices = new Uint32Array([
-      0, 2, 6, 6, 2, 4, // +Z face
-      1, 7, 3, 3, 7, 5, // -Z face
-      // 0, 1, 3, 3, 1, 2
+      // 0, 2, 6, 6, 2, 4, // +Z face
+      // 1, 7, 3, 3, 7, 5, // -Z face
+      0, 1, 3, 3, 1, 2
     ]);
 
     // prettier-ignore
     const vertexNormals = new Float32Array([
       // +Z face  -Z face
-         0, 0, 1, 0, 0, -1, // v0
-         0, 0, 1, 0, 0, -1, // v1
-         0, 0, 1, 0, 0, -1, // v2
-         0, 0, 1, 0, 0, -1, // v3
+         0, 0, 1, /* 0, 0, -1, */ // v0
+         0, 0, 1, /* 0, 0, -1, */ // v1
+         0, 0, 1, /* 0, 0, -1, */ // v2
+         0, 0, 1, /* 0, 0, -1, */ // v3
     ]);
 
     this.setVertices(vertices);
