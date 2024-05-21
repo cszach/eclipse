55cda580fa52a9067c07c12a7a6e7d13bc2ae9cd
Zach <duong.nguyen@rochester.edu>
2024-05-21 09:30:24 -0400

    feat(renderer/utils): Create utils to extract data from scene and viewport

---
+++ src/renderers/utils/SceneUtils.ts
@@ -0,0 +1,148 @@
+import {mat4, quat, vec3} from 'wgpu-matrix';
+import {Mesh, Scene} from '../../primitives/exports.js';
+import {
+  BLINN_PHONG,
+  BlinnPhong,
+  Lambert,
+  Metal,
+  METAL,
+  SolidColor,
+} from '../../materials/exports.js';
+import {Light} from '../../lights/exports.js';
+
+type SceneData = {
+  vertexData: Float32Array;
+  indexData: Uint32Array;
+  materialData: Float32Array;
+  lightData: Float32Array;
+};
+
+class SceneUtils {
+  static getData(scene: Scene, updateStats = false): SceneData {
+    if (updateStats && scene.stats.outdated) {
+      scene.updateStats();
+    }
+
+    const vertexData = new Float32Array(scene.stats.vertices * 12);
+    const indexData = new Uint32Array(scene.stats.triangles * 4);
+    const materialData = new Float32Array(scene.stats.meshes * 8);
+    const lightData = new Float32Array(scene.stats.lights * 8);
+
+    let vertexDataOffset = 0;
+    let indexDataOffset = 0;
+    let materialDataOffset = 0;
+    let materialIndex = 0;
+    let lightDataOffset = 0;
+    let numVerticesProcessed = 0;
+
+    scene.traverse((group, globalPosition, globalRotation, globalScale) => {
+      const worldMatrix = mat4.translation(globalPosition);
+      const {angle, axis} = quat.toAxisAngle(globalRotation);
+      mat4.rotate(worldMatrix, axis, angle, worldMatrix);
+      mat4.scale(worldMatrix, globalScale, worldMatrix);
+
+      const worldMatrixInverseTranspose = mat4.invert(worldMatrix);
+      mat4.transpose(worldMatrixInverseTranspose, worldMatrixInverseTranspose);
+
+      if (group instanceof Mesh) {
+        const mesh = group;
+
+        // Material
+
+        let color = [0, 0, 0];
+        let specular = [0, 0, 0];
+        let shininess = 0;
+
+        if (mesh.material.type < 5) {
+          const coloredMaterial = mesh.material as
+            | SolidColor
+            | BlinnPhong
+            | Lambert
+            | Metal;
+
+          color = coloredMaterial.color;
+        }
+
+        if (mesh.material.type === BLINN_PHONG) {
+          const blinnPhongMaterial = mesh.material as BlinnPhong;
+
+          specular = blinnPhongMaterial.specular;
+          shininess = blinnPhongMaterial.shininess;
+        }
+
+        if (mesh.material.type === METAL) {
+          const metalMaterial = mesh.material as Metal;
+
+          shininess = metalMaterial.fuzziness;
+        }
+
+        materialData[materialDataOffset++] = color[0];
+        materialData[materialDataOffset++] = color[1];
+        materialData[materialDataOffset++] = color[2];
+        materialData[materialDataOffset++] = shininess;
+        materialData[materialDataOffset++] = specular[0];
+        materialData[materialDataOffset++] = specular[1];
+        materialData[materialDataOffset++] = specular[2];
+        materialData[materialDataOffset++] = mesh.material.type;
+
+        // Vertices
+
+        mesh.geometry.forEachTriangle((_index, indices) => {
+          indexData[indexDataOffset++] = indices[0] + numVerticesProcessed;
+          indexData[indexDataOffset++] = indices[1] + numVerticesProcessed;
+          indexData[indexDataOffset++] = indices[2] + numVerticesProcessed;
+          indexData[indexDataOffset++] = 0; // Pad
+        });
+
+        mesh.geometry.forEachVertex((_index, position, normal, uv) => {
+          const transformedPosition = vec3.transformMat4(position, worldMatrix);
+
+          const transformedNormal = vec3.transformMat4(
+            normal,
+            worldMatrixInverseTranspose
+          );
+
+          vertexData[vertexDataOffset++] = transformedPosition[0];
+          vertexData[vertexDataOffset++] = transformedPosition[1];
+          vertexData[vertexDataOffset++] = transformedPosition[2];
+          vertexData[vertexDataOffset++] = 0; // Pad
+          vertexData[vertexDataOffset++] = transformedNormal[0];
+          vertexData[vertexDataOffset++] = transformedNormal[1];
+          vertexData[vertexDataOffset++] = transformedNormal[2];
+          vertexData[vertexDataOffset++] = 0; // Pad
+          vertexData[vertexDataOffset++] = uv[0];
+          vertexData[vertexDataOffset++] = uv[1];
+          vertexData[vertexDataOffset++] = materialIndex;
+          vertexData[vertexDataOffset++] = 0; // Pad
+
+          numVerticesProcessed++;
+        });
+
+        materialIndex++;
+      }
+
+      // Light
+      if (group instanceof Light) {
+        const light = group;
+
+        const transformedLightPosition = vec3.transformMat4(
+          [0, 0, 0],
+          worldMatrix
+        );
+
+        lightData[lightDataOffset++] = transformedLightPosition[0]; // 0
+        lightData[lightDataOffset++] = transformedLightPosition[1];
+        lightData[lightDataOffset++] = transformedLightPosition[2];
+        lightData[lightDataOffset++] = light.intensity;
+        lightData[lightDataOffset++] = light.color[0]; // 16
+        lightData[lightDataOffset++] = light.color[1];
+        lightData[lightDataOffset++] = light.color[2];
+        lightData[lightDataOffset++] = light.type;
+      }
+    });
+
+    return {vertexData, indexData, materialData, lightData};
+  }
+}
+
+export {SceneUtils, SceneData};
---
+++ src/renderers/utils/ViewportUtils.ts
@@ -0,0 +1,55 @@
+import {Vec3, vec3} from 'wgpu-matrix';
+import {PerspectiveCamera} from '../../cameras/exports.js';
+import {UP} from '../../constants.js';
+
+type ViewportData = {
+  origin: Vec3;
+  du: Vec3;
+  dv: Vec3;
+};
+
+class ViewportUtils {
+  static getData(
+    camera: PerspectiveCamera,
+    canvas: HTMLCanvasElement
+  ): ViewportData {
+    const lookAt = [0, 0, 0]; // TODO: do not hard-code lookAt
+    const focalLength = vec3.length(
+      vec3.subtract(camera.localPosition, lookAt)
+    );
+    const h = Math.tan(camera.verticalFovRadians / 2);
+    const viewportHeight = 2 * h * focalLength;
+    const viewportWidth = viewportHeight * camera.aspectRatio;
+
+    // Opposite of camera direction
+    const w = vec3.normalize(vec3.subtract(camera.localPosition, lookAt));
+    const normalizedU = vec3.normalize(vec3.cross(UP, w)); // Local right
+    const normalizedV = vec3.cross(w, normalizedU); // Local up
+
+    const u = vec3.mulScalar(normalizedU, viewportWidth);
+    const v = vec3.mulScalar(vec3.negate(normalizedV), viewportHeight);
+
+    const du = vec3.divScalar(u, canvas.width);
+    const dv = vec3.divScalar(v, canvas.height);
+
+    const halfU = vec3.divScalar(u, 2);
+    const halfV = vec3.divScalar(v, 2);
+
+    // viewportUpperLeft = cameraPos - (w * focalLength) - U / 2 - V / 2
+    const viewportUpperLeft = vec3.sub(
+      vec3.sub(
+        vec3.sub(camera.localPosition, vec3.mulScalar(w, focalLength)),
+        halfU
+      ),
+      halfV
+    );
+
+    const pixelDuv = vec3.add(du, dv);
+    const halfPixelDuv = vec3.mulScalar(pixelDuv, 0.5);
+    const origin = vec3.add(viewportUpperLeft, halfPixelDuv);
+
+    return {origin, du, dv};
+  }
+}
+
+export {ViewportUtils, ViewportData};
