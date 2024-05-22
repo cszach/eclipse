af0f0a8215a3defce3b1e409b2fa7df9cc5a3c10
Zach <duong.nguyen@rochester.edu>
2024-05-22 14:05:54 -0400

    refactor(renderers): :coffin: Remove dead renderer constants.
    BREAKING CHANGE: the constants vertexBufferLayout, hlbvhConstructionBindGroupLayoutDescriptor, and rayTracingBindGroupLayoutDescriptor are now gone.

--- src/renderers/Rasterizer.ts
+++ src/renderers/Rasterizer.ts
@@ -3,7 +3,6 @@ import {PerspectiveCamera} from '../cameras/exports.js';
 import {UP} from '../constants.js';
 import {Light} from '../lights/Light.js';
 import {Renderer} from './Renderer.js';
-import {vertexBufferLayout} from './constants.js';
 
 // Shaders
 import solidColorShader from '../materials/shaders/solid_color.wgsl';
@@ -471,6 +470,44 @@ class Rasterizer implements Renderer {
     const module = this.device.createShaderModule({
       code: solidColorShader + blinnPhongShader + rasterizerShader,
     });
+
+    // The vertex buffer layout
+    //
+    //  Data: | Position X| Position Y| Position Z|
+    // Bytes: | 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|…
+    //        |   Normal X|   Normal Y|   Normal Z|
+    //        |12|13|14|15|16|17|18|19|20|21|22|23|…
+    //        |          U|          V|MaterialIdx|
+    //        |24|25|26|27|28|29|30|31|32|33|34|35|
+    const vertexBufferLayout: GPUVertexBufferLayout = {
+      arrayStride: 9 * 4,
+      attributes: [
+        {
+          // position
+          format: 'float32x3',
+          offset: 0,
+          shaderLocation: 1,
+        },
+        {
+          // vertex normals
+          format: 'float32x3',
+          offset: 12,
+          shaderLocation: 2,
+        },
+        {
+          // UV
+          format: 'float32x2',
+          offset: 24,
+          shaderLocation: 3,
+        },
+        {
+          // material index
+          format: 'float32',
+          offset: 32,
+          shaderLocation: 4,
+        },
+      ],
+    };
 
     this.pipeline = this.device.createRenderPipeline({
       label: 'Rasterizer pipeline',
